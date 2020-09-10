e3eab188332d119857413554b8dfb6d901ec609e
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-10 13:48:37 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,2 @@
 array = ["fiddleheads", "okra", "kohlrabi"]
 
-def oxford_comma
-  if array.length == 1 
-    return array[0]
-    elsif
-    array.length == 2 
-    return array.join
-    
-
-  end
-end

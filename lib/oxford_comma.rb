493d24a4fd8281e77cdfef1e051cb1d9160133e8
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-10 13:48:49 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,2 +1,15 @@
 array = ["fiddleheads", "okra", "kohlrabi"]
 
+def oxford_comma(array)
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length >= 3
+    array[-1] = "and #{array[-1]}"
+
+    return array.join(",")
+end
+end
+
+puts oxford_comma(array)
